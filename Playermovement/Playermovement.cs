dddc31bd17f9e58aff5229bb73843ca4855100e5
@OfficialCharlieChaplin <124716301+Bossofchibi@users.noreply.github.com>
2023-08-13 16:58:24 -0400

    Update Playermovement.cs

--- Playermovement/Playermovement.cs
+++ Playermovement/Playermovement.cs
@@ -25,28 +25,44 @@ public class PlayerMovement : MonoBehaviour
     Rigidbody rb;
 
     private void start()
+    
     {
-    rb = GetComponent<Rigidbody>();
-    rb.freezeRotation = true;
+        rb = GetComponent<Rigidbody>();
+        rb.freezeRotation = true;
     }
+    
     private void update()
+   
     {
-    MyInput();
+
+        grounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.5f + 0.2f, whatIsGround);
+    
+        MyInput();
+
+        if(grounded)
+        rb.drag = groundDrag;
+        else
+        rb.drag = 0
     }
+   
     private void FixedUpdate()
+   
     {
-    MovePlayer();
+        MovePlayer();
     }
-    private void Myinput()
+    
+     private void Myinput()
+    
     {
-    horizontalInput = Input.GetAxisRaw("Horizontal");
-    verticalInput = Input.GetAxisRaw("Vertical");
+        horizontalInput = Input.GetAxisRaw("Horizontal");
+        verticalInput = Input.GetAxisRaw("Vertical");
     }
 
-    private void MovePlayer()
+     private void MovePlayer()
+   
     {
-    moveDirection = orientation forward * verticalInput + orientation.right * horizontal input;
+        moveDirection = orientation forward * verticalInput + orientation.right * horizontal input;
 
-    rb.AddForce(moveDirection.normalized * moveSpeed * 10f, ForceMode.Force);
+        rb.AddForce(moveDirection.normalized * moveSpeed * 10f, ForceMode.Force);
     }
 }
